9db90cb7dc492c7b45a5aad6598bde726ddcf97f
Vasudev <kingzvasu660@gmail.com>
2021-10-01 01:09:11 +0530

    Dark Mode Enabled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+import {useState} from 'react';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import Textbox from './components/Textbox';
 function App() {
+  const [darkMode, setdarkMode] = useState('light');
+  const [textVal, setTextVal] = useState('Enable Dark Mode')
+  const toggleMode = () => {
+    if(darkMode==='light'){
+    setdarkMode('dark')
+    document.body.style.backgroundColor = '#11085c';
+    setTextVal('Enable Light Mode')
+    }
+    else{
+      setdarkMode('light')
+      setTextVal('Enable Dark Mode')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar mode={darkMode} toggleMode = {toggleMode} text={textVal}/>
       <div className="container">
-      <Textbox heading = "Enter The Text To Analyze Below"/>
+      <Textbox heading = "Enter The Text To Analyze Below" mode={darkMode}/>
+      {/* <About/> */}
       </div>
     </div>
   );
--- src/components/Textbox.js
+++ src/components/Textbox.js
@@ -13,15 +13,38 @@ export default function Textbox(props) {
     const handleEvent = (event) => {
         setText(event.target.value);
     }
+    
+    const handleClear = () =>{
+        setText('');
+    }
+
+    const handleCopy = () => {
+        var text = document.getElementById("myBox");
+        text.select();
+        // text.setSelectionRange(0,9999);
+        navigator.clipboard.writeText(text.value);
+        alert("Text Copied");
+    }
+
+    const handleSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
 
     var t = 0.008 *text.split(" ").length
     t = t.toFixed(4);
     return (
         <div>
-            <div className="container">
+            <div className="container" style = {{
+                backgroundColor: props.mode==='dark' ? '#11085c' : 'white',
+                color:props.mode==='dark' ? 'white' : '#11085c'
+            }}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleEvent} id="myBox" rows="5"></textarea>
+            <textarea className="form-control" style = {{
+                backgroundColor: props.mode==='dark' ? 'grey' : 'white',
+                color:props.mode==='dark' ? 'white' : '#11085c'
+            }} value={text} onChange={handleEvent} id="myBox" rows="5"></textarea>
             </div>
 
             <button className="btn btn-primary mx-5 my-5" onClick={handleClick}>
@@ -30,9 +53,17 @@ export default function Textbox(props) {
             <button className="btn btn-primary mx-5" onClick={handleClicktoLower}>
             Convert to Lowercase
             </button>
+            <button className="btn btn-primary mx-5" onClick={handleCopy}>
+            Copy Text
+            </button>
+            <button className="btn btn-danger mx-5" onClick={handleSpaces}>Clear Extra Spaces</button>
+            <button className="btn btn-danger mx-5" onClick={handleClear}>Clear Text</button>
             </div>
 
-            <div className="container my-5">
+            <div className="container my-5" style = {{
+                backgroundColor: props.mode==='dark' ? '#11085c' : 'white',
+                color:props.mode==='dark' ? 'white' : '#11085c'
+            }}>
                 <h2>
                     Your Text Summary
                 </h2>
@@ -48,7 +79,7 @@ export default function Textbox(props) {
                     Preview
                 </h2>
                 <h4>
-                    {text}
+                    {text.length>0?text:"Enter Some text in the text Box"}
                 </h4>
             </div>
         </div>
